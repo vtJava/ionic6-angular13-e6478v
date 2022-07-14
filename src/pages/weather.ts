42c54311cda44f1543081d3c440a51e2a720942d
valtaust <valtaust93@gmail.com>
2022-07-14 18:13:12 +0100

    1 file modified

--- src/pages/weather.ts
+++ src/pages/weather.ts
@@ -6,72 +6,71 @@ import { CityProvider } from '../services/city-provider.service';
 
 @Component({
   selector: 'weather',
-  templateUrl:'./weather.html',
-  providers: [ WeatherProvider, CityProvider ],
+  templateUrl: './weather.html',
+  providers: [WeatherProvider, CityProvider],
 })
 export class WeatherComponent implements OnInit {
   @Input() name: string;
 
-    private _storage: Storage | null = null;
-  
-    city: String = 'Dublin';
-    cityName: any = {};
-    weatherInfo: any = {};
-    units: string;
-    lat: string;
-    lng: string;
-    weather = true;
-    
-    constructor(
-      public http: HttpClient,public navCtrl: NavController,public wp: WeatherProvider,
-      private storage: Storage
-    ){
-      http
+  private _storage: Storage | null = null;
+
+  city: String = 'Dublin';
+  cityName: any = {};
+  weatherInfo: any = {};
+  units: string;
+  lat: string;
+  lng: string;
+  weather = true;
+
+  constructor(
+    public http: HttpClient,
+    public navCtrl: NavController,
+    public wp: WeatherProvider,
+    private storage: Storage
+  ) {
+    http
       .get('https://restcountries.com/v3.1/capital/Dublin/')
-      .subscribe((weather) => (this.weather));
-    }
-  
-    async ngOnInit() {
-      this.weatherInfo.getWeather().subscribe(response =>
-        this.weatherInfo=response.weatherInfo)
-      // If using, define drivers here: await this.storage.defineDriver(/*...*/);
-      const storage = await this.storage.create(this.weatherInfo);
-      this.storage = storage;
-    }
-  
-    // Create and expose methods that users of this service can
-    // call, for example:
-    public set(key: string, value: any) {
-      this._storage?.set(key, value);
-    }
-  
-    ionViewDidEnter() {
-      //boolean load a method as true and else a false to show false
-      //load this provider  to use the import
-      //retrieve the storage data with this function
-      this.storage.get('cityName').then((value) => {
-        //Storage of City (capital)
-        if (value == null) {
-          this.weather = true;
-          console.log('No City value in storage');
-        } else {
-          this.weather = false;
-          this.city = value;
-          console.log(value);
-        }
-      });
-  
-      this.storage.get('weatherInfo').then((value) => {
-        //Storage of weather info
-        if (value == null) {
-          console.log('No Info found');
-        } else {
-          this.weatherInfo = value;
-        }
-      });
-    }
-  
-    ionViewDidLoad() {
-      //todo
-    }
+      .subscribe((weather) => this.weather);
   }
+
+  async ngOnInit() {
+    this.weatherInfo
+      .getWeather()
+      .subscribe((response) => (this.weatherInfo = response.weatherInfo));
+    // If using, define drivers here: await this.storage.defineDriver(/*...*/);
+    const storage = await this.storage.create(this.weatherInfo);
+    this.storage = storage;
+  }
+
+  // Create and expose methods that users of this service can
+  // call, for example:
+  public set(key: string, value: any) {
+    this._storage?.set(key, value);
+  }
+
+  ionViewWillEnter() {
+    //boolean load a method as true and else a false to show false
+    //load this provider  to use the import
+    //retrieve the storage data with this function
+    this.storage.get('cityName').then((value) => {
+      //Storage of City (capital)
+      if (value == null) {
+        this.weather = true;
+        console.log('No City value in storage');
+      } else {
+        this.weather = false;
+        this.city = value;
+        console.log(value);
+      }
+    });
+
+    this.storage.get('weatherInfo').then((value) => {
+      //Storage of weather info
+      if (value == null) {
+        console.log('No Info found');
+      } else {
+        this.weatherInfo = value;
+      }
+    });
+  }
+}
